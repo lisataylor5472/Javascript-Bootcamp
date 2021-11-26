0a40bebb50bc4a64dc7fd2877440eda7c0e6b5bc
Kyle Liu <kyle.liu@ssaihq.com>
2021-11-25 23:03:36 -0800

    added lesson 1

---
+++ lesson1/fundamentals.js
@@ -0,0 +1,51 @@
+// Exercise 1
+
+
+
+
+
+
+
+// Exercise 2
+
+
+
+
+
+
+
+
+// Exercise 3
+
+
+
+
+
+
+
+
+
+// Exercise 4
+// Write an if statement that will log the text "That is far!"
+// if the "distance" variable is greater than 25
+let distance = 27;
+
+
+
+
+// Write an if statement that will log "Small", "Medium", or "Large"
+// based on the "size" variable value, "S" should log the word "Small"
+let size = 's';
+
+
+
+
+
+
+
+
+// Write an if statement that checks 
+// if the value of "jerseyNumber" is equal to 23 AND "jerseyColor" equal to "yellow"
+// log "LeBron James" if true
+let jerseyNumber = 23;
+let jerseyColor = "yellow";
