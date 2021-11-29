96739ce534b6a0e0fbd8ed404d1de2f6f9027862
Kyle Liu <kyle.liu@ssaihq.com>
2021-11-28 20:08:17 -0800

    lesson4 added

---
+++ lesson3/functions.js
@@ -0,0 +1,25 @@
+// Exercise 1
+// Write a function declaration
+
+
+// Write a function expression
+
+
+// Write a function using an "arrow" function
+
+
+
+
+
+// Create a function that will take 2 numbers and log the sum of the 2 numbers
+
+
+
+
+// Create a function that takes in a number and logs from 0 to whatever number that is passed in
+
+
+
+
+
+// Create a function that takes in a number and logs if it is even or not
